2f4533c72eb9528ebcba7874fe95a4eda7c26547
id2913 <violette.bebin@eleve.ensai.fr>
2026-04-15 13:24:29 +0000

    fgfggvffcggh

--- src/Model/match.py
+++ src/Model/match.py
@@ -1,13 +1,25 @@
-from datetime import date
-from team import Equipe
-
 class Match:
-    """
-    Classe définissant un match entre deux équipes à une date donnée
-    """
-    def __init__(self, id: int, date:datetime.date, equipe_1: Equipe, equipe_2: Equipe, stats: dict) -> None:
-            self._id = id
-            self._date = date
-            self._equipe_1 = equipe_1
-            self._equipe_2 = equipe_2
-            self._stats = stats
+
+    def __init__(self, id, date, team1, team2, score1, score2):
+        self.date = date
+        self.team1 = team1
+        self.team2 = team2
+        self.score1 = score1
+        self.score2 = score2
+
+    def winner(self):
+        if self.score1 > self.score2:
+            return self.team1
+        elif self.score2 > self.score1:
+            return self.team2
+        return None
+
+    def __str__(self):
+        return f"{self.date} | {self.team1} {self.score1} - {self.score2} {self.team2}"
+
+    def __repr__(self):
+        return (
+            f"Match(date='{self.date}', "
+            f"team1='{self.team1}', team2='{self.team2}', "
+            f"score1={self.score1}, score2={self.score2}')"
+        )
