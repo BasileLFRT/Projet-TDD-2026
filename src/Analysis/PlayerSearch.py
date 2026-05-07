339ae478e604754859a0b2d87824074a82a983f4
BasileLFRT <basile.laferte@eleve.ensai.fr>
2026-05-07 19:50:12 +0200

    rien du tout

--- src/Analysis/PlayerSearch.py
+++ src/Analysis/PlayerSearch.py
@@ -2,11 +2,6 @@ from typing import List
 
 from src.Model.Player import Player
 
-# Note for students :
-# This class could be a static class, it does not explicitly need a constructor.
-# It could also be simply a module that exports the methods and removes the class altogether because Python doesn't require everything to be an object!
-
-
 class PlayerSearch:
     def filter_players_by_full_name(
         self, players: List[Player], search_string: str
@@ -14,3 +9,7 @@ class PlayerSearch:
         # This syntax is called "list comprehension" in Python, and can be handy for simple filtering like this :)
         search_words = set(search_string.lower().split())
         return [player for player in players if set(player.nom.lower().split()) == search_words]
+    
+# Note for students :
+# This class could be a static class, it does not explicitly need a constructor.
+# It could also be simply a module that exports the methods and removes the class altogether because Python doesn't require everything to be an object!
